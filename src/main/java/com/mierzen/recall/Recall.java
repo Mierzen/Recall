f9b3bca166eced5f6701558b4d5b1ca669439d02
Mierzen <steptaljaard@gmail.com>
2015-08-12 14:17:31 +0200

    Add proxies

--- src/main/java/com/mierzen/recall/Recall.java
+++ src/main/java/com/mierzen/recall/Recall.java
@@ -1,17 +1,37 @@
 package com.mierzen.recall;
 
-import net.minecraft.init.Blocks;
+import com.mierzen.recall.proxies.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = References.MODID, name = References.NAME, version = References.VERSION)
 public class Recall
 {
+    @Mod.Instance
+    public static Recall instance = new Recall();
+
+    @SidedProxy(clientSide="com.mierzen.recall.proxies.ClientProxy", serverSide="com.mierzen.recall.proxies.ServerProxy")
+    public static CommonProxy proxy;
+
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void preInit(FMLPreInitializationEvent e)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        proxy.preInit(e);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e)
+    {
+        proxy.init(e);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+        proxy.postInit(e);
     }
 }
