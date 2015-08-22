36e2990b490b2985f89746e8ebaa3c6621593f02
Mierzen <steptaljaard@gmail.com>
2015-08-22 12:57:10 +0200

    Move Teleport class into MessageTeleport

--- src/main/java/com/mierzen/recall/MessageTeleport.java
+++ src/main/java/com/mierzen/recall/MessageTeleport.java
@@ -3,6 +3,8 @@ package com.mierzen.recall;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
 
 /**
@@ -37,7 +39,45 @@ public class MessageTeleport extends MessageBase<MessageTeleport>
 
         System.out.println("message called");
 
-        Teleport tp = new Teleport();
-        tp.tryTeleport(world, (EntityPlayerMP) player);
+
+        //timers (ticks)
+        double channelTime = 0;
+        double cooldownTime = 0;
+
+        player.addChatMessage(new ChatComponentText("Cooldown: " + cooldownTime));
+
+        System.out.println("trying  to teleport");
+        if (!player.isSneaking())
+        {
+            if (cooldownTime == 0.0F)
+            {
+                if (performTeleport(world, (EntityPlayerMP)player))
+                    cooldownTime = 150;
+            }
+        }
+    }
+
+    public boolean performTeleport(World world, EntityPlayer player)
+    {
+        ChunkCoordinates destination = player.getBedLocation(world.provider.dimensionId); //getBedSpawnPosition
+
+        if (destination == null)
+        {
+            destination = /*world.getSpawnPoint();*/world.provider.getSpawnPoint();
+            destination.posY = world.getHeightValue(destination.posX, destination.posY);
+        }
+
+        if (destination != null)
+            //player.addChatMessage(new ChatComponentText("World remote: " + world.isRemote + " || " + destination.toString()));
+
+            if (!player.worldObj.isRemote)
+            {
+                player.setPositionAndUpdate(destination.posX - 0.5F, destination.posY + 1, destination.posZ - 0.5F);
+                //player.addChatMessage(new ChatComponentText("TP:  World remote: " + world.isRemote + " || " + player.worldObj.isRemote));
+
+                return Boolean.TRUE;
+            }
+
+        return Boolean.FALSE;
     }
 }
